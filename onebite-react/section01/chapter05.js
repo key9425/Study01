2efc09efb8efadd276130b5ad6324974564800fb
김은영 <kimeunyoung-0425@naver.com>
2025-08-04 20:46:39 +0900

    내용 정리 및 수정

--- onebite-react/section01/chapter05.js
+++ onebite-react/section01/chapter05.js
@@ -1,7 +1,7 @@
 // 1. Number Type
-let num1 = 27; // 정수
-let num2 = 1.5; // 실수
-let num = -20; // 음수
+let num1 = 27;
+let num2 = 1.5;
+let num = -20;
 
 let int = Infinity; // 양의 무한대
 let mInf = -Infinity; // 음의 무한대
@@ -9,11 +9,11 @@ let nan = NaN; // not a number, 수치연산이 실패했을 때의 결과값으
 // console.log(1 * "hello"); // 출력 : NaN -> 불가능한 수치 연산 시 프로그램이 종료되지 않음
 
 // 1-1. 기본적인 사칙연산 지원
-console.log(num1 + num2); // 덧셈
-console.log(num1 - num2); // 뺄셈
-console.log(num1 * num2); // 곱셈
-console.log(num1 / num2); // 나눗셈
-console.log(num1 % num2); // 나머지 (모듈러 연산)
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 % num2);
 
 // --------------------------------------------------------------------------
 
@@ -22,8 +22,7 @@ let myName = "이정환"; // " or ' 사용 -> ", '를 사용하지 않으면 변
 
 // 2-1. 덧셈 연산 지원
 let myLocation = "목동";
-let introduce = myName + myLocation;
-// console.log(introduce); // 출력 : 이정환목동
+let introduce = myName + myLocation; // 이정환목동
 
 // 2-2. 템플릿 리터럴 문법
 let introduceText = `${myName}은 ${myLocation}에 거주합니다`; // `(backtick) 사용 => ${}를 사용하면 변수값을 동적으로 문자열에 포함시킬 수 있다
--- onebite-react/section01/chapter06.js
+++ onebite-react/section01/chapter06.js
@@ -1,11 +1,8 @@
 // 1. 묵시적 형 변환
 // -> 자바스크립트 엔진이 알아서 형 변환 하는 것
-
 let num = 10;
 let str = "20";
-
-const result = num + str;
-console.log(result); // 출력 : 1020
+const result = num + str; // 1020
 
 // ---------------------------------------------
 
@@ -20,10 +17,8 @@ console.log(10 + strToNum1); // 출력 : 20
 // 2-2 숫자 외에 다른 값이 포함되어 있는 문자열 값을 숫자 값으로 형 변환
 // 주의) 문자가 앞에 있을 경우 형 변환이 일어나지 않을 수도 있음
 let str2 = "10개";
-// let strToNum2 = Number(str2);
-// console.log(strToNum2); // 출력 : NaN
-let strToNum2 = parseInt(str2);
-console.log(strToNum2); // 출력 : 10
+let strToNum2 = parseInt(str2); // 10
+// let strToNum2 = Number(str2); // NaN
 
 // 2-3. 숫자 -> 문자열
 let num1 = 20;
